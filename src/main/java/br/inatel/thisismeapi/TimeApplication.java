959885de02a73db7e8a3c25b480b2e36dabc059b
GabrielHS <gabrielhenri0012@gmail.com>
2022-08-27 09:54:30 -0300

    project base

---
+++ src/main/java/br/inatel/thisismeapi/TimeApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.thisismeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeApplication.class, args);
+	}
+
+}
