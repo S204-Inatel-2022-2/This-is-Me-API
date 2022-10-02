b6be89b93b360206f22a047181b87c6f83a6af0a
GabrielHS <gabrielhenri0012@gmail.com>
2022-10-02 12:08:07 -0300

    ajuste para retornar msg erro

--- src/main/java/br/inatel/thisismeapi/security/CustomUserDetailsService.java
+++ src/main/java/br/inatel/thisismeapi/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package br.inatel.thisismeapi.security;
 
 import br.inatel.thisismeapi.entities.User;
 import br.inatel.thisismeapi.repositories.UserRepository;
+import br.inatel.thisismeapi.utils.UserUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         LOGGER.info("m=loadUserByUsername, username={}", username);
+
+        String validEmail = UserUtils.verifyEmail(username);
+        if (validEmail != null) {
+            throw new RuntimeException(validEmail);
+        }
+
         Optional<User> opUser = userRepository.findByEmail(username);
 
         if (opUser.isEmpty())
